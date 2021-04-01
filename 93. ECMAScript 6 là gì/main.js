a934cc649b811409a92f567d068f1867491ca514
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-04-01 21:25:37 +0700

    Bai 93

---
+++ 93. ECMAScript 6 là gì/main.js
@@ -0,0 +1,13 @@
+
+// 1. Let, const
+// 2. Template Literals
+// 3. Multi-line String
+// 4. Arrow function
+// 5. Classes
+// 6. Default paramter values
+// 7. Destructuring
+// 8. Rest paramters
+// 9. Spread
+// 10. Enhanced object liternals
+// 11. Tagged template literal
+// 12. Modules
