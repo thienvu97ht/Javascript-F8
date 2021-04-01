ed25d9a51346400e08f7a04eb48d7f47c088e0ba
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-04-01 22:30:27 +0700

    Bai 95

---
+++ 95. Template literals/main.js
@@ -0,0 +1,28 @@
+
+// 1. Let, const
+// 2. Template Literals
+// 3. Multi-line String
+// 4. Arrow function
+// 5. Classes
+// 6. Default paramter values
+// 7. Destructuring
+// 8. Rest paramters
+// 9. Spread
+// 10. Enhanced object liternals
+// 11. Tagged template literal
+// 12. Modules
+
+// Ví dụ 1: Template Literals
+const courseName = 'Javascript';
+const courseName2 = 'PHP';
+const description = `Course Name: ${courseName} ${courseName2}`;
+
+console.log(description); // Course Name: Javascript PHP
+
+// Ví dụ 2: 
+const lines = `Line 1
+Line 2
+Line 3`;
+
+
+console.log(lines);
